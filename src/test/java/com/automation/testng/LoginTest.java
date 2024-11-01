a92fcfca4f3e8497cfa9417174f91c329a55ef1e
jesna-susan <jesnasusanreji@gmail.com>
2024-11-02 01:09:09 +0530

    Modified classes in testng with asserts

--- src/test/java/com/automation/testng/LoginTest.java
+++ src/test/java/com/automation/testng/LoginTest.java
@@ -1,6 +1,8 @@
 package com.automation.testng;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class LoginTest extends BaseTest {
     @Test(groups = {"Smoke"})
@@ -8,8 +10,19 @@ public class LoginTest extends BaseTest {
         System.out.println("Test Case 1 login");
     }
 
+    @Test(groups={"Smoke"})
+    public void verifyUseCanLogout(){
+        System.out.println("Test case 2 login");
+        SoftAssert softAssert =new SoftAssert();
+        softAssert.assertEquals("10","11");
+        softAssert.assertAll();
+
+    }
+
     @Test(groups = {"Regression"})
     public void verifyUserCannotLoginWithInvalidCred(){
-        System.out.println("Test Case 2 login");
+        System.out.println("Test Case 3 login");
+        Assert.assertTrue("Test".equals("Test"), "Expected and actual");
+        Assert.assertEquals("gh","hg");
     }
 }
